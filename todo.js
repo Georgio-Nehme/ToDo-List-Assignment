3f28ae8e238d726d8998588922eacab32e67b9cb
georgio-nehme <georgionehme@gmail.com>
2022-06-03 16:01:14 +0300

    Recreated the js file (tring a new approach)

--- todo.js
+++ todo.js
@@ -1,15 +1,13 @@
 
 //DOM//
-var completed = document.getElementById("completed_tasks");
+var completed = $("#completed_tasks");
+var title_input = $("#title");
+var importance_input = $("#importance_input");
+var todo_input = $("#todo_content");
+const add_button = $("#add_button");
 
-var title_input = document.getElementById("title");
-var importance_input = document.getElementById("importance_input");
-var todo_input = documet.getElementById("todo_content");
 
-const add_buuton = document.getElementById("add_button");
-
-// ToDo creator //
-class todo {
+class Todo {
   constructor(id,title,content,importance,is_done) {
     this.id = id 
     this.title = title;
@@ -17,73 +15,95 @@ class todo {
     this.importance=importance;
     this.is_done= is_done;
   }
+
+  setId(new_id){
+    this.id = new_id;
+  }
+  setTitle(new_title){
+    this.title=new_title;
+  }
+  setContent(new_content){
+    this.content=new_content;
+  }
+  setImportance(new_importance){
+    this.importance=new_importance;
+  }
+  done(){
+    this.is_done=true;
+  }
+
+
+  getId(){
+    return this.id;
+  }
+  getTitle(){
+    return this.title;
+  }
+  getImportance(){
+    return this.importance;
+  }
 }
 
-var todo_list = [];
 
-add_buuton.addEventListener("click",function(id, title, content, importance, is_done){
-
-var todo = new todo(todo_list.length + 1, title_input.val(),todo_input.val(),importance_input.val(),false);
+// var todo1 = new Todo(1,"maintenance","go to the mechanical check",1,false);
 
 
-todo_list.push(todo);
+// todo1.setTitle("Go Home");
+// todo1.setContent("DriveDriveDriveDrive");
+// todo1.setImportance(5);
+// todo1.done();
+// console.log(todo1)
 
-})
+$("#add_button").click(function() {
+  let todo = new Todo(-1," "," "," ",false);
+  todo.setTitle(title_input.val());
+  todo.setContent(todo_input.val());
+  todo.setImportance(importance_input.val());
+
+  console.log(todo.getImportance);
+});
 
 
 
+
+
+
+
+
+// var todo_list = [];
+
+// add_buuton.addEventListener("click",function(id, title, content, importance, is_done){
+
+// var todo = new todo(todo_list.length + 1, title_input.val(),todo_input.val(),importance_input.val(),false);
+
+
+// todo_list.push(Todo);
+
+// })
 
 
 
 //On click events //
-
-
-
-
-
-
-
 //listing the todos//
-window.onload = function listTodos(){
-  for(i=0;i<localStorage.getItem(ToDoList).length;i++){
-    let todo = localStorage.getItem(ToDoList)[i];
-    let title = todo[title];
-    let importance = todo[importance];
-    let content = todo[content];
-    let is_done = todo[is_done]
-  }
-}
+// window.onload = function listTodos(){
+//   for(i = 0 ; i <localStorage.getItem(ToDoList).length;i++){
+//     let todo = localStorage.getItem(ToDoList)[i];
+//     let title = todo[title];
+//     let importance = todo[importance];
+//     let content = todo[content];
+//     let is_done = todo[is_done]
+//   }
+// }
 
 // console.log(todo)
-
-
-
-
-
-
-
-
-
-
 
  ////////////
 //Functions//
 ////////////
 
-
-
-
-
-
-
-
-
 // 
-
-// var todo1 = new todo(1,"maintenance","go to the mechanical check",5,false);
 // var todo2 = new todo(2,"maintenance","go to the mechanical check",3,false);
 // var todo3 = new todo(3,"go to haven","gogogogogogogogogogogogogo",2,false);
-
 
 // var todo_list = [];
 
@@ -91,27 +111,13 @@ window.onload = function listTodos(){
 // todo_list.push(todo2);
 // todo_list.push(todo3);
 
-
 // localStorage.setItem("ToDoList" ,todo_list);
 
-
 // var ToDoList = localStorage.getItem("ToDoList");
-
-
-
-
-
-
-
-
 
 // function listTodos () {
 //   for( let i = 0; i< length())
 // };
-
-
-
-
 
 
 // function myFunction() {
